f97df7dea01ddee1017336a6f3d781ab0daea8a7
Miguel Prada <mapra99@gmail.com>
2019-08-19 16:36:50 -0500

    Added my_each method

--- ennMethods.rb
+++ ennMethods.rb
@@ -1,3 +1,15 @@
+# Module
 module Enumerable
-  
+  #my_each: clone of each
+  def my_each
+    for(i in 1...self.length)
+      yield(self[i])
+    end
+  end
+end
+
+# Methods Practice
+include Enumerable
+my_each(arr) do |x|
+  puts x
 end
