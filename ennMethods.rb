5e91dfdfe097acc82caab122d899ce5a32a6a9ec
Miguel Prada <mapra99@gmail.com>
2019-08-19 17:30:23 -0500

    Moved Exercises to a new file

--- ennMethods.rb
+++ ennMethods.rb
@@ -63,27 +63,3 @@ module Enumerable
     return counter
   end
 end
-
-# Methods Practice
-include Enumerable
-arr = [1,5,84,2,4,1,6,5,8]
-
-arr.my_each do |x|
-  puts x
-end
-
-arr.my_each_with_index do |x, index|
-  puts "value: #{x} with index: #{index}"
-end
-
-arr.my_select do |value|
-  value.odd?
-end
-
-arr.my_all? do |x|
-  x.odd?
-end
-
-arr.my_any? do |x|
-  x.odd?
-end
