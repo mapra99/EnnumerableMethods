863eb38ab4eac435be2d0f20cf82f04c1e29b3f1
Miguel Prada <mapra99@gmail.com>
2019-08-19 16:49:45 -0500

    Added my_select method

--- ennMethods.rb
+++ ennMethods.rb
@@ -2,15 +2,27 @@
 module Enumerable
   #my_each: clone of each
   def my_each
-    for(i in 1...self.length)
+    for i in 0...self.length do
       yield(self[i])
     end
   end
 
   #my_each_with_index: clone of each.with_index
   def my_each_with_index
-    for(i in 1...self.length)
+    for i in 0...self.length do
       yield(self[i], i)
+    end
+  end
+
+  #my_select: clone of select
+  def my_select
+    i = 0
+    result = []
+    self.my_each do |x|
+      if yield(self[i])
+        result[i] = self[i]
+        i += 1
+      end
     end
   end
 end
@@ -18,6 +30,7 @@ end
 # Methods Practice
 include Enumerable
 arr = [1,5,84,2,4,1,6,5,8]
+
 arr.my_each do |x|
   puts x
 end
