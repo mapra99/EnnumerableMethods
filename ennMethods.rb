62fd61349f5ebc643fb101adf017f024511b5e53
Miguel Prada <mapra99@gmail.com>
2019-08-19 16:44:30 -0500

    Added my_each_with_index method. Fixed some bug in my_each method

--- ennMethods.rb
+++ ennMethods.rb
@@ -6,10 +6,22 @@ module Enumerable
       yield(self[i])
     end
   end
+
+  #my_each_with_index: clone of each.with_index
+  def my_each_with_index
+    for(i in 1...self.length)
+      yield(self[i], i)
+    end
+  end
 end
 
 # Methods Practice
 include Enumerable
-my_each(arr) do |x|
+arr = [1,5,84,2,4,1,6,5,8]
+arr.my_each do |x|
   puts x
+end
+
+ arr.my_each_with_index do |x, index|
+  puts "value: #{x} with index: #{index}"
 end
